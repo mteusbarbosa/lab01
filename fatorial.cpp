631d9d0ce21c771d0e28fa2b79ba564aea1d72e1
mteusbarbosa <mateus.bm13@gmail.com>
2017-08-18 02:16:36 +0000

    Deletado alguns arquivos

--- fatorial.cpp
+++
@@ -1,8 +0,0 @@
-#include "fatorial.h"
-
-int fatorial(int n){
-    if (n < 1) return -1; //passo base
-    if (n == 0 || n ==1 ) return n;
-    if (n == 2) return n;
-    else return n * fatorial(n-1);
-}
